d376a12c0d888cca4ed7736a5b519482c6b95422
Glabay <Java_Guru@live.com>
2024-09-17 03:06:31 -0300

    Moves the TradePost to the applicable packaging

--- src/main/java/io/xeros/model/world/objects/actions/areas/varrack/market/TradingPost.java
+++ src/main/java/io/xeros/model/world/objects/actions/areas/varrack/market/TradingPost.java
@@ -1,4 +1,4 @@
-package io.xeros.model.world.objects.actions.areas.home;
+package io.xeros.model.world.objects.actions.areas.varrack.market;
 
 import io.xeros.content.tradingpost.Listing;
 import io.xeros.model.entity.player.Player;
