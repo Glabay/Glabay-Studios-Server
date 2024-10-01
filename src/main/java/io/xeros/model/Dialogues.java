5f1aaf44b58a1cab8fa9fe6bf7e3fcaaa052ae83
Glabay <Java_Guru@live.com>
2024-10-01 15:07:09 -0300

    Creates a Static class to hold Dialogue ID's

---
+++ src/main/java/io/xeros/model/Dialogues.java
@@ -0,0 +1,12 @@
+package io.xeros.model;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project Glabay-Studios-Server
+ * @social Discord: Glabay
+ * @since 2024-10-01
+ */
+public class Dialogues {
+
+    public static final Integer EMBLEM_TRADER_DIALOGUE = 545;
+}
