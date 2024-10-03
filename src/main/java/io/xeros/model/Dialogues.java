d37f41cb6d1ffa2221342cfcb8e96d4d7e57aea3
Zeighe <zeighezag420@outlook.com>
2024-10-03 17:02:52 -0500

    2024/10/03:  Added new dialogue ID's

--- src/main/java/io/xeros/model/Dialogues.java
+++ src/main/java/io/xeros/model/Dialogues.java
@@ -12,4 +12,5 @@ public class Dialogues {
     public static final Integer PORT_OFFICIAL_DIALOGUE = 55870;
     public static final Integer PORT_WORKER_DIALOGUE = 55869;
     public static final Integer ZEAH_SOLDIER_DIALOGUE = 55877;
+    public static final Integer GERTRUDE_DIALOGUE = 64;
 }
