708ae34b595c1df6b6dabd2844bb8877285a841a
Glabay <Java_Guru@live.com>
2024-10-04 10:57:58 -0300

    Adds logging to the PacketType.java

--- src/main/java/io/xeros/net/packets/PacketType.java
+++ src/main/java/io/xeros/net/packets/PacketType.java
@@ -1,7 +1,10 @@
 package io.xeros.net.packets;
 
 import io.xeros.model.entity.player.Player;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public interface PacketType {
+	Logger logger = LoggerFactory.getLogger(PacketType.class);
 	void processPacket(Player c, int packetType, int packetSize);
 }
