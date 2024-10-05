bbf3e5f7b9332204c340fc0dcc7b99af5f240091
Zeighe <zeighezag420@outlook.com>
2024-10-05 11:01:22 -0500

    2024/10/05:  Adds Interfaces.java & Shops.java

---
+++ src/main/java/io/xeros/model/Interfaces.java
@@ -0,0 +1,13 @@
+package io.xeros.model;
+
+/**
+ * @author Zei | Glabay-Studios
+ * @project Glabay-Studios-Server
+ * @social Discord: Z
+ * @social Github: <a href="https://github.com/Zeighe">Zeighe</a>
+ * @since 10/5/2024
+ */
+public class Interfaces {
+
+    public static final Integer MAKEOVER_MAGE_INTERFACE = 3559;
+}
---
+++ src/main/java/io/xeros/model/Shops.java
@@ -0,0 +1,16 @@
+package io.xeros.model;
+
+/**
+ * @author Zei | Glabay-Studios
+ * @project Glabay-Studios-Server
+ * @social Discord: Z
+ * @social Github: <a href="https://github.com/Zeighe">Zeighe</a>
+ * @since 10/5/2024
+ */
+public class Shops {
+
+    public static final Integer MASTER_FARMER_SHOP = 16;
+    public static final Integer NURMOF_SHOP = 90;
+    public static final Integer SIGMUND_THE_MERCHANT_SHOP = 26;
+    public static final Integer HUNTER_SHOP = 23;
+}
