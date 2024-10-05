3a0376810480a09309b387bb9ef044b86f8b79a1
Zeighe <zeighezag420@outlook.com>
2024-10-05 09:15:27 -0500

    2024/10/05:  Updated location to Canifis vs Morytania

--- src/main/java/io/xeros/model/entity/npc/interactions/impl/canifis/LeSabre.java
+++ src/main/java/io/xeros/model/entity/npc/interactions/impl/canifis/LeSabre.java
@@ -1,11 +1,10 @@
-package io.xeros.model.entity.npc.interactions.impl.morytania;
+package io.xeros.model.entity.npc.interactions.impl.canifis;
 
 import io.xeros.model.entity.npc.NPC;
 import io.xeros.model.entity.npc.interactions.NpcOptionAction;
 import io.xeros.model.entity.player.Player;
 
 import static io.xeros.model.Npcs.LE_SABR;
-import static io.xeros.model.Npcs.TOBY;
 
 /**
  * @author Zei | Glabay-Studios
