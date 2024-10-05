5411a3ad9fb97a45ab21e38de0a4a060a1ae7b5a
Zeighe <zeighezag420@outlook.com>
2024-10-05 09:15:50 -0500

    2024/10/05:  Adds several dialogues

--- src/main/java/io/xeros/model/Dialogues.java
+++ src/main/java/io/xeros/model/Dialogues.java
@@ -15,4 +15,11 @@ public class Dialogues {
     public static final Integer GERTRUDE_DIALOGUE = 64;
     public static final Integer KEY_MASTER_DIALOGUE = 105;
     public static final Integer KEY_MASTER_DIALOGUE_2 = 3305;
+    public static final Integer LILLIA_DIALOGUE = 105;
+    public static final Integer COMBAT_INSTRUCTOR_DIALOGUE = 1390;
+    public static final Integer ZUL_ARETH_DIALOGUE = 637;
+    public static final Integer MAGE_OF_ZAMORAK_DIALOGUE = 629;
+    public static final Integer KRYSTILIA_DIALOGUE = 3299;
+    public static final Integer KRYSTILIA_2_DIALOGUE = 3304;
+    public static final Integer MAZCHNA_DIALOGUE = 3300;
 }
