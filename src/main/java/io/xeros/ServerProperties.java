807953e30c822b9a7ce40b5c93d2f85e5cfd1698
Glabay <Java_Guru@live.com>
2024-10-05 10:36:35 -0300

    Housekeeping of ServerProperties

--- src/main/java/io/xeros/ServerProperties.java
+++ src/main/java/io/xeros/ServerProperties.java
@@ -1,61 +1,65 @@
 package io.xeros;
 
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Locale;
 import java.util.Properties;
 
 public class ServerProperties {
+    private static final Logger logger = LoggerFactory.getLogger(ServerProperties.class);
 
     public enum PropType {
         SERVER,
         CACHE
     }
 
+    @Getter
     private static final Properties serverProperties = new Properties();
+    @Getter
     private static final Properties cacheProperties = new Properties();
 
-    public static Properties getServerProperties() {
-        return serverProperties;
-    }
-
-    public static Properties getCacheProperties() {
-        return cacheProperties;
-    }
-
     public static void loadProperties(PropType type) {
-        File propFile = new File("./" + type.name().toLowerCase(Locale.getDefault()) + ".properties");
-        try {
-            if (!propFile.exists()) {
-                propFile.createNewFile();
-            }
+        var propFile = new File("./" + type.name().toLowerCase(Locale.getDefault()) + ".properties");
+        try (var fis = new FileInputStream(propFile)) {
+            if (!propFile.exists())
+                if (!propFile.createNewFile())
+                    logger.error("Error creating missing properties file: {}", propFile.getAbsolutePath());
+
             switch (type) {
                 case CACHE:
-                    cacheProperties.load(new FileInputStream(propFile));
-                    File assetsLink = new File("./assetslink.txt");
+                    cacheProperties.load(fis);
+                    var assetsLink = new File("./assetslink.txt");
                     if (assetsLink.exists()) {
-                        String cachePath = new String(java.nio.file.Files.readAllBytes(assetsLink.toPath())).replace("cache\\", "");
+                        var cachePath = new String(Files.readAllBytes(assetsLink.toPath())).replace("cache\\", "");
                         cacheProperties.setProperty("cache", cachePath);
                         saveProperties(type, cacheProperties);
-                        assetsLink.delete();
+                        if (!assetsLink.delete())
+                            logger.error("Error deleting Cache Prop File: {}", assetsLink.getAbsolutePath());
                     }
                     break;
                 case SERVER:
-                    serverProperties.load(new FileInputStream(propFile));
+                    serverProperties.load(fis);
                     break;
             }
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace(System.err); // Consider a more robust error handling strategy
         }
     }
 
     public static void saveProperties(PropType type, Properties p) {
-        String path = "./" + type.name().toLowerCase(Locale.getDefault()) + ".properties";
-        try (FileOutputStream fr = new FileOutputStream(path)) {
+        var path = "./%s.properties".formatted(type.name().toLowerCase(Locale.getDefault()));
+        try (var fr = new FileOutputStream(path)) {
             p.store(fr, "Properties");
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace(System.err); // Consider a more robust error handling strategy
         }
     }
