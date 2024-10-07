db69b634398932174d3786851ee8a9bea9cd6412
Zeighe <zeighezag420@outlook.com>
2024-10-06 23:33:27 -0500

    2024/10/06:  Adds plugin for Vannaka

--- src/main/java/io/xeros/model/Shops.java
+++ src/main/java/io/xeros/model/Shops.java
@@ -12,6 +12,7 @@ public class Shops {
     public static final Integer MASTER_FARMER_SHOP = 16;
     public static final Integer GRACE_SHOP = 18;
     public static final Integer CRAFTING_SHOP = 20;
+    public static final Integer FISHING_SHOP = 22;
     public static final Integer HUNTER_SHOP = 23;
     public static final Integer SIGMUND_THE_MERCHANT_SHOP = 26;
     public static final Integer PEST_CONTROL_SHOP = 75;
