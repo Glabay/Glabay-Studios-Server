689d24b7af3e19a0a58d41fac3a1b8fb354559d1
Zeighe <zeighezag420@outlook.com>
2024-10-07 20:00:53 -0500

    2024/10/07: Adds plugin for Menaphite Thug

--- src/main/java/io/xeros/net/packets/npcoptions/NpcOptionTwo.java
+++ src/main/java/io/xeros/net/packets/npcoptions/NpcOptionTwo.java
@@ -62,9 +62,6 @@ public class NpcOptionTwo {
                 logger.error("Unhandled NPC Action 2: {} ", npcAction.getClass().getSimpleName());
         }
         switch (npcType) {
-            case 3550:
-                player.getThieving().steal(Thieving.Pickpocket.MENAPHITE_THUG, npc);
-                break;
             case 6094:
                 player.getThieving().steal(Thieving.Pickpocket.GNOME, npc);
                 break;
