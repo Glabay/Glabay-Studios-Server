079193747e303f4e501486be0a1ddc8202da7a55
Glabay <Java_Guru@live.com>
2024-10-26 00:13:49 -0300

    Code Cleanup

--- src/main/java/io/xeros/content/combat/CombatConfigs.java
+++ src/main/java/io/xeros/content/combat/CombatConfigs.java
@@ -18,20 +18,14 @@ public class CombatConfigs {
      * @return The combat style for the Npc
      */
     private static CombatStyle getCombatStyleForNpc(NpcCombatDefinition definition) {
-        switch(definition.getAttackStyle()) {
-            case "Stab":
-                return CombatStyle.STAB;
-            case "Slash":
-                return CombatStyle.SLASH;
-            case "Crush":
-                return CombatStyle.CRUSH;
-            case "Ranged":
-                return CombatStyle.RANGE;
-            case "Magic":
-                return CombatStyle.MAGIC;
-            default:
-                return CombatStyle.SPECIAL;
-        }
+        return switch (definition.getAttackStyle()) {
+            case "Stab" -> CombatStyle.STAB;
+            case "Slash" -> CombatStyle.SLASH;
+            case "Crush" -> CombatStyle.CRUSH;
+            case "Ranged" -> CombatStyle.RANGE;
+            case "Magic" -> CombatStyle.MAGIC;
+            default -> CombatStyle.SPECIAL;
+        };
     }
 
     /**
@@ -40,28 +34,21 @@ public class CombatConfigs {
      * @return The combat style for the entity type
      */
     public static CombatStyle getCombatStyle(Entity entity) {
-        if (entity.isNPC()) {
-            NPC n = entity.asNPC();
-            NpcCombatDefinition definition = NpcCombatDefinition.definitions.get(n.getNpcId());
+        if (entity instanceof NPC n) {
+            var definition = NpcCombatDefinition.definitions.get(n.getNpcId());
             if (definition != null) {
                 return getCombatStyleForNpc(definition);
             }
             return CombatStyle.SPECIAL;
-        } else if (entity.isPlayer()) {
-            Player player = entity.asPlayer();
-
-            switch (getCombatType(player)) {
-                case MAGE:
-                    return CombatStyle.MAGIC;
-                case RANGE:
-                    return CombatStyle.RANGE;
-                default:
-                    return player.getCombatConfigs().getWeaponMode().getCombatStyle();
-            }
-
-        } else {
-            throw new IllegalArgumentException("You cannot use that entity type here. ");
         }
+        else if (entity instanceof Player player) {
+            return switch (getCombatType(player)) {
+                case MAGE -> CombatStyle.MAGIC;
+                case RANGE -> CombatStyle.RANGE;
+                default -> player.getCombatConfigs().getWeaponMode().getCombatStyle();
+            };
+        }
+        else throw new IllegalArgumentException("You cannot use that entity type here. ");
     }
 
     /**
@@ -70,13 +57,12 @@ public class CombatConfigs {
      * @return The combat type of the player
      */
     public static CombatType getCombatType(Player player) {
-        if (player.usingMagic) {
+        if (player.usingMagic)
             return CombatType.MAGE;
-        } else if (player.usingBow || player.usingOtherRangeWeapons || player.usingCross || player.usingBallista) {
+        else if (player.usingBow || player.usingOtherRangeWeapons || player.usingCross || player.usingBallista)
             return CombatType.RANGE;
-        } else {
+        else
             return CombatType.MELEE;
-        }
     }
 
 }
