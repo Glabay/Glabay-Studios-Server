5dc67d672d3a5e47ec7624647e4bc9d52054f7d3
Glabay <Java_Guru@live.com>
2024-10-26 00:12:03 -0300

    Code Cleanup

--- src/main/java/io/xeros/content/combat/death/NPCDeath.java
+++ src/main/java/io/xeros/content/combat/death/NPCDeath.java
@@ -38,213 +38,182 @@ import io.xeros.util.Misc;
 public class NPCDeath {
 
     public static void dropItems(NPC npc) {
-        Player c = PlayerHandler.players[npc.killedBy];
-        if (c != null) {
-            dropItemsFor(npc, c, npc.getNpcId());
-        }
+        var player = PlayerHandler.players[npc.killedBy];
+        if (player != null)
+            dropItemsFor(npc, player, npc.getNpcId());
     }
 
-    public static void dropItemsFor(NPC npc, Player c, int npcId) {
-        if (c.getTargeted() != null && npc.equals(c.getTargeted())) {
-            c.setTargeted(null);
-            c.getPA().sendEntityTarget(0, npc);
+    public static void dropItemsFor(NPC npc, Player player, int npcId) {
+        if (player.getTargeted() != null && npc.equals(player.getTargeted())) {
+            player.setTargeted(null);
+            player.getPA().sendEntityTarget(0, npc);
         }
-        c.getAchievements().kill(npc);
-        PetHandler.rollOnNpcDeath(c, npc);
-        if (npcId >= 1610 && npcId <= 1612) {
-            //	c.setArenaPoints(c.getArenaPoints() + 1);
-            c.getQuestTab().updateInformationTab();
-            //	c.sendMessage("@red@You gain 1 point for killing the Mage! You now have " + c.getArenaPoints()
-            //+ " Arena Points.");
-        }
+        player.getAchievements().kill(npc);
+        PetHandler.rollOnNpcDeath(player, npc);
+        //	player.setArenaPoints(player.getArenaPoints() + 1);
+        //	player.sendMessage("@red@You gain 1 point for killing the Mage! You now have " + player.getArenaPoints()
+        //+ " Arena Points.");
+        if (npcId >= 1610 && npcId <= 1612) player.getQuestTab().updateInformationTab();
 
-        if (npcId == 2266 || npcId == 2267 || npcId == 2265) {
-            c.getDiaryManager().getFremennikDiary().progress(FremennikDiaryEntry.KILL_DAGANNOTH_KINGS);
-        }
-        if (npcId == 411) {
-            c.getDiaryManager().getFremennikDiary().progress(FremennikDiaryEntry.KILL_KURASK);
-        }
+        if (npcId == 2266 || npcId == 2267 || npcId == 2265)
+            player.getDiaryManager().getFremennikDiary().progress(FremennikDiaryEntry.KILL_DAGANNOTH_KINGS);
+        if (npcId == 411)
+            player.getDiaryManager().getFremennikDiary().progress(FremennikDiaryEntry.KILL_KURASK);
 
         if (npcId == 9021 || npcId == 9022 || npcId == 9023 || npcId == 9024) {
-            c.hunllefDead = true;
-            //PlayerHandler.executeGlobalMessage("@red@[EVENT]@blu@ Hunllef has been defeated by @bla@" + c.playerName + "@blu@!");
-            Hunllef.rewardPlayers(c);
+            player.hunllefDead = true;
+            //PlayerHandler.executeGlobalMessage("@red@[EVENT]@blu@ Hunllef has been defeated by @bla@" + player.playerName + "@blu@!");
+            Hunllef.rewardPlayers(player);
         }
-        if (npcId == 1047) {
-            c.getDiaryManager().getMorytaniaDiary().progress(MorytaniaDiaryEntry.KILL_CAVE_HORROR);
-        }
+        if (npcId == 1047)
+            player.getDiaryManager().getMorytaniaDiary().progress(MorytaniaDiaryEntry.KILL_CAVE_HORROR);
 
-        if (npcId == 1673) { //barrows npcs
-            Achievements.increase(c, AchievementType.DH_KILLS, 1);
-        }
+        //barrows npcs
+        if (npcId == 1673)
+            Achievements.increase(player, AchievementType.DH_KILLS, 1);
         if (npcId == 5744 || npcId == 5762) {
-            c.setShayPoints(c.getShayPoints() + 1);
-            c.sendMessage("@red@You gain 1 point for killing the Penance! You now have " + c.getShayPoints()
-                    + " Assault Points.");
+            player.setShayPoints(player.getShayPoints() + 1);
+            player.sendMessage("@red@You gain 1 point for killing the Penance! You now have %d Assault Points.".formatted(player.getShayPoints()));
         }
 
-        if (npc.getNpcId() == 3162 || npc.getNpcId() == 3129 || npc.getNpcId() == 2205 || npc.getNpcId() == 2215) {
-            c.getEventCalendar().progress(EventChallenge.KILL_X_GODWARS_BOSSES_OF_ANY_TYPE);
-        }
-        if (npc.getNpcId() == 9293) {
-            c.getEventCalendar().progress(EventChallenge.KILL_BASILISK_KNIGHTS_X_TIMES);
-        }
+        if (npc.getNpcId() == 3162 || npc.getNpcId() == 3129 || npc.getNpcId() == 2205 || npc.getNpcId() == 2215)
+            player.getEventCalendar().progress(EventChallenge.KILL_X_GODWARS_BOSSES_OF_ANY_TYPE);
+        if (npc.getNpcId() == 9293)
+            player.getEventCalendar().progress(EventChallenge.KILL_BASILISK_KNIGHTS_X_TIMES);
 
-        if (npc.getNpcId() == 2042 || npc.getNpcId() == 2043 || npc.getNpcId() == 2044) {
-            c.getEventCalendar().progress(EventChallenge.KILL_ZULRAH_X_TIMES);
-        }
-        if (npc.getNpcId() == 9021) {
-            c.getEventCalendar().progress(EventChallenge.KILL_HUNLLEF_X_TIMES);
-        }
-        if (IntStream.of(DropManager.wildybossesforgiveaway).anyMatch(id -> id == npc.getNpcId()) && c.getPosition().inWild()) {
-            c.getEventCalendar().progress(EventChallenge.KILL_X_WILDY_BOSSES);
-        }
-        if (npcId >= 7931 && npcId <= 7940) {
-            c.getEventCalendar().progress(EventChallenge.KILL_X_REVS_IN_WILDY);
-        }
+        if (npc.getNpcId() == 2042 || npc.getNpcId() == 2043 || npc.getNpcId() == 2044)
+            player.getEventCalendar().progress(EventChallenge.KILL_ZULRAH_X_TIMES);
+        if (npc.getNpcId() == 9021)
+            player.getEventCalendar().progress(EventChallenge.KILL_HUNLLEF_X_TIMES);
+        if (IntStream.of(DropManager.wildybossesforgiveaway).anyMatch(id -> id == npc.getNpcId()) && player.getPosition().inWild())
+            player.getEventCalendar().progress(EventChallenge.KILL_X_WILDY_BOSSES);
+        if (npcId >= 7931 && npcId <= 7940)
+            player.getEventCalendar().progress(EventChallenge.KILL_X_REVS_IN_WILDY);
 
-        if (npcId == Npcs.CORPOREAL_BEAST) {
-            NPCHandler.kill(Npcs.DARK_ENERGY_CORE, npc.heightLevel);
-        }
-        if (npcId == 7278) {
-            if ((c.getSlayer().getTask().isPresent() && c.getSlayer().getTask().get().getPrimaryName().equals("nechryael"))) {
-                c.getPA().addSkillXPMultiplied(100, Skill.SLAYER.getId(), true);
-            }
-        }
+        if (npcId == Npcs.CORPOREAL_BEAST) NPCHandler.kill(Npcs.DARK_ENERGY_CORE, npc.heightLevel);
+        if (npcId == 7278)
+            if ((player.getSlayer().getTask().isPresent() && player.getSlayer().getTask().get().getPrimaryName().equals("nechryael")))
+                player.getPA().addSkillXPMultiplied(100, Skill.SLAYER.getId(), true);
+        if (npcId == Npcs.DUSK_7888)
+            Achievements.increase(player, AchievementType.GROTESQUES, 1);
+        if (npcId == Npcs.ALCHEMICAL_HYDRA_8621)
+            Achievements.increase(player, AchievementType.HYDRA, 1);
 
-        if (npcId == Npcs.DUSK_7888) {
-            Achievements.increase(c, AchievementType.GROTESQUES, 1);
-        }
-        if (npcId == Npcs.ALCHEMICAL_HYDRA_8621) {
-            Achievements.increase(c, AchievementType.HYDRA, 1);
-        }
+        if (npcId == 2266 || npcId == 2267 || npcId == 2265)
+            if ((player.getSlayer().getTask().isPresent() && player.getSlayer().getTask().get().getPrimaryName().equals("dagannoth")))
+                player.getPA().addSkillXPMultiplied(165, Skill.SLAYER.getId(), true);
 
-        if (npcId == 2266 || npcId == 2267 || npcId == 2265) {
-            if ((c.getSlayer().getTask().isPresent() && c.getSlayer().getTask().get().getPrimaryName().equals("dagannoth"))) {
-                c.getPA().addSkillXPMultiplied(165, Skill.SLAYER.getId(), true);
-            }
+        if (npcId == 1673 || npcId == 1674 || npcId == 1677 || npcId == 1676 || npcId == 1675 || npcId == 1672) {//barrows
+            Achievements.increase(player, AchievementType.BARROWS_KILLS, 1);
+            player.getEventCalendar().progress(EventChallenge.KILL_X_BARROWS_BROTHERS);
+            if (EquipmentSet.AHRIM.isWearingBarrows(player) || EquipmentSet.KARIL.isWearingBarrows(player)
+                    || EquipmentSet.DHAROK.isWearingBarrows(player)
+                    || EquipmentSet.VERAC.isWearingBarrows(player)
+                    || EquipmentSet.GUTHAN.isWearingBarrows(player)
+                    || EquipmentSet.TORAG.isWearingBarrows(player))
+                player.getDiaryManager().getMorytaniaDiary().progress(MorytaniaDiaryEntry.BARROWS_CHEST);
         }
+        if (npcId == 7144 || npcId == 7145 || npcId == 7146)
+            player.getEventCalendar().progress(EventChallenge.KILL_X_DEMONIC_GORILLAS, 1);
 
-        if (npcId == 1673 || npcId == 1674 || npcId == 1677 || npcId == 1676 || npcId == 1675
-                || npcId == 1672) {//barrows
-            Achievements.increase(c, AchievementType.BARROWS_KILLS, 1);
-            c.getEventCalendar().progress(EventChallenge.KILL_X_BARROWS_BROTHERS);
-            if (EquipmentSet.AHRIM.isWearingBarrows(c) || EquipmentSet.KARIL.isWearingBarrows(c)
-                    || EquipmentSet.DHAROK.isWearingBarrows(c)
-                    || EquipmentSet.VERAC.isWearingBarrows(c)
-                    || EquipmentSet.GUTHAN.isWearingBarrows(c)
-                    || EquipmentSet.TORAG.isWearingBarrows(c)) {
-                c.getDiaryManager().getMorytaniaDiary().progress(MorytaniaDiaryEntry.BARROWS_CHEST);
-            }
-        }
-        if (npcId == 7144 || npcId == 7145 || npcId == 7146) {
-            c.getEventCalendar().progress(EventChallenge.KILL_X_DEMONIC_GORILLAS, 1);
-        }
-
-        if (npcId == 1739 || npcId == 1740 || npcId == 1741 || npcId == 1742)
-        {
-            c.getEventCalendar().progress(EventChallenge.GAIN_X_PEST_CONTROL_POINTS, 7);
-            c.pcPoints += 7;
+        if (npcId == 1739 || npcId == 1740 || npcId == 1741 || npcId == 1742) {
+            player.getEventCalendar().progress(EventChallenge.GAIN_X_PEST_CONTROL_POINTS, 7);
+            player.pcPoints += 7;
         }
 
         if (npcId == FragmentOfSeren.NPC_ID && npc.getHealth().getCurrentHealth() <= 0) {
             // Player p = PlayerHandler.players[npc.killedBy];
             int randomPkp = Misc.random(15) + 10;
-            c.pkp += randomPkp;
-            c.getQuestTab().updateInformationTab();
+            player.pkp += randomPkp;
+            player.getQuestTab().updateInformationTab();
             MonsterHunt.setCurrentLocation(null);
-            c.sendMessage("Well done! You killed Seren!");
-            c.sendMessage("You received: " + randomPkp + " PK Points for killing the wildy boss.");
+            player.sendMessage("Well done! You killed Seren!");
+            player.sendMessage("You received: " + randomPkp + " PK Points for killing the wildy boss.");
 
         }
 
         if (npcId == TheUnbearable.NPC_ID && npc.getHealth().getCurrentHealth() <= 0) {
             int randomPkp = Misc.random(15) + 10;
-            c.pkp += randomPkp;
-            c.getQuestTab().updateInformationTab();
+            player.pkp += randomPkp;
+            player.getQuestTab().updateInformationTab();
             MonsterHunt.setCurrentLocation(null);
-            c.sendMessage("Well done! You killed The Unbearable!");
-            c.sendMessage("You received: " + randomPkp + " PK Points for killing the wildy boss.");
+            player.sendMessage("Well done! You killed The Unbearable!");
+            player.sendMessage("You received: " + randomPkp + " PK Points for killing the wildy boss.");
         }
 
         if (npcId == Hespori.NPC_ID && npc.getHealth().getCurrentHealth() <= 0) {
-            c.getQuestTab().updateInformationTab();
-            c.sendMessage("Well done! You killed Hespori!");
+            player.getQuestTab().updateInformationTab();
+            player.sendMessage("Well done! You killed Hespori!");
         }
         int dropX = npc.absX;
         int dropY = npc.absY;
         int dropHeight = npc.heightLevel;
 
-        if (!PathFinder.getPathFinder().accessable(c, dropX, dropY)) {
-            for (Position border : npc.getBorder()) {
-                if (PathFinder.getPathFinder().accessable(c, dropX, dropY)) {
-                    dropX = border.getX();
-                    dropY = border.getY();
-                    break;
-                }
+        if (!PathFinder.getPathFinder().accessable(player, dropX, dropY)) for (Position border : npc.getBorder())
+            if (PathFinder.getPathFinder().accessable(player, dropX, dropY)) {
+                dropX = border.getX();
+                dropY = border.getY();
+                break;
             }
-        }
 
         if (npcId == 492 || npcId == 494 || npcId == NightmareConstants.NIGHTMARE_ACTIVE_ID || npcId == Npcs.VORKATH) {
-            dropX = c.absX;
-            dropY = c.absY;
+            dropX = player.absX;
+            dropY = player.absY;
         }
         if (npcId == 2042 || npcId == 2043 || npcId == 2044
                 || npcId == 6720) {
             dropX = 2268;
             dropY = 3069;
-            c.getItems().addItem(12938, 1);
-            c.getZulrahEvent().stop();
+            player.getItems().addItem(12938, 1);
+            player.getZulrahEvent().stop();
         }
         if (npcId == Kraken.KRAKEN_ID) {
             dropX = 2280;
             dropY = 10031;
         }
 
-        /**
-         * Warriors guild
-         */
-        c.getWarriorsGuild().dropDefender(npc.absX, npc.absY);
-        if (AnimatedArmour.isAnimatedArmourNpc(npcId)) {
-            if (npc.getX() == 2851 && npc.getY() == 3536) {
-                dropX = 2851;
-                dropY = 3537;
-                AnimatedArmour.dropTokens(c, npcId, npc.absX, npc.absY + 1);
-            } else if (npc.getX() == 2857 && npc.getY() == 3536) {
-                dropX = 2857;
-                dropY = 3537;
-                AnimatedArmour.dropTokens(c, npcId, npc.absX, npc.absY + 1);
-            } else {
-                AnimatedArmour.dropTokens(c, npcId, npc.absX, npc.absY);
-            }
+        player.getWarriorsGuild().dropDefender(npc.absX, npc.absY);
+        if (AnimatedArmour.isAnimatedArmourNpc(npcId)) if (npc.getX() == 2851 && npc.getY() == 3536) {
+            dropX = 2851;
+            dropY = 3537;
+            AnimatedArmour.dropTokens(player, npcId, npc.absX, npc.absY + 1);
         }
+        else if (npc.getX() == 2857 && npc.getY() == 3536) {
+            dropX = 2857;
+            dropY = 3537;
+            AnimatedArmour.dropTokens(player, npcId, npc.absX, npc.absY + 1);
+        }
+        else
+            AnimatedArmour.dropTokens(player, npcId, npc.absX, npc.absY);
 
 
         Location3D location = new Location3D(dropX, dropY, dropHeight);
         int amountOfDrops = 1;
-        if (isDoubleDrops()) {
+        if (isDoubleDrops())
             amountOfDrops++;
-        }
-
         int bossPoints = BossPoints.getPointsOnDeath(npc);
-        BossPoints.addPoints(c, bossPoints, false);
+        BossPoints.addPoints(player, bossPoints, false);
 
-        if (NpcDef.forId(npcId).getCombatLevel() >= 1) {
-            c.getNpcDeathTracker().add(NpcDef.forId(npcId).getName(), NpcDef.forId(npcId).getCombatLevel(), bossPoints);
-        }
+        if (NpcDef.forId(npcId).getCombatLevel() >= 1)
+            player.getNpcDeathTracker().add(NpcDef.forId(npcId).getName(), NpcDef.forId(npcId).getCombatLevel(), bossPoints);
 
-        Server.getDropManager().create(c, npc, location, amountOfDrops, npcId);
+        Server.getDropManager().create(player, npc, location, amountOfDrops, npcId);
     }
 
     public static void announce(Player player, GameItem item, int npcId) {
-        if (!player.getDisplayName().equalsIgnoreCase("thimble") && !player.getDisplayName().equalsIgnoreCase("top hat")) {
-            announceKc(player, item, player.getNpcDeathTracker().getKc(NpcDef.forId(npcId).getName()));
-        }
+        announceKc(player, item, player.getNpcDeathTracker().getKc(NpcDef.forId(npcId).getName()));
     }
 
     public static void announceKc(Player player, GameItem item, int kc) {
-        PlayerHandler.executeGlobalMessage("@pur@" + player.getDisplayNameFormatted() + " received a drop: " +
-                "" + ItemDef.forId(item.getId()).getName() + " x " + item.getAmount() + " at <col=E9362B>" + kc  + "</col>@pur@ kills.");
+        PlayerHandler.executeGlobalMessage(
+            "@pur@%s received a drop: %s x %d at <col=E9362B>%d</col>@pur@ kills."
+            .formatted(
+                player.getDisplayNameFormatted(),
+                ItemDef.forId(item.getId()).getName(),
+                item.getAmount(),
+                kc
+            )
+        );
     }
 
     public static boolean isDoubleDrops() {
