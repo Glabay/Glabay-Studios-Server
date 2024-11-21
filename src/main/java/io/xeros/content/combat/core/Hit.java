4d207827c231229f2a3b20ae57c230ed35272817
Glabay <Java_Guru@live.com>
2024-11-21 14:14:49 -0400

    Adds a Hitmark as a param for Hit

--- src/main/java/io/xeros/content/combat/core/Hit.java
+++ src/main/java/io/xeros/content/combat/core/Hit.java
@@ -7,19 +7,25 @@ import io.xeros.model.entity.Entity;
 
 public class Hit {
     final Entity attacker, target;
+    public Hitmark hitType;
     public int damage, delay;
+
+    public Hit(Entity attacker, Entity target, int damage, Hitmark hitType) {
+        this(attacker, target, damage, 0);
+        this.hitType = hitType;
+    }
 
     public Hit(Entity attacker, Entity target, int damage, int delay) {
         this.attacker = attacker;
         this.target = target;
         this.damage = damage;
         this.delay = delay;
+        this.hitType = damage > 0 ? Hitmark.HIT : Hitmark.MISS;
     }
 
     public Hit submit() {
         if (this.target == null) return this;
-        Hitmark hitmark = this.damage > 0 ? Hitmark.HIT : Hitmark.MISS;
-        Damage damage = new Damage(this.attacker, this.target, this.damage, this.delay, hitmark, CombatType.RANGE);
+        Damage damage = new Damage(this.attacker, this.target, this.damage, this.delay, hitType, CombatType.RANGE);
         damage.getTarget().appendDamage(damage.getAmount(), damage.getHitmark());
         System.out.println("delay: " + this.delay);
         System.out.println("adding damage: " + damage);
