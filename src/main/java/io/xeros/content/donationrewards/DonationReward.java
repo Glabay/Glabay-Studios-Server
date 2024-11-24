5ed766e8ac44fddd9cb07a93ad18249dfdf75dcb
Glabay <Java_Guru@live.com>
2024-11-24 12:15:56 -0400

    Upgrade: converts Donation Reward to a Record

--- src/main/java/io/xeros/content/donationrewards/DonationReward.java
+++ src/main/java/io/xeros/content/donationrewards/DonationReward.java
@@ -11,13 +11,13 @@ import io.xeros.model.items.GameItem;
 import io.xeros.util.JsonUtil;
 import lombok.Getter;
 
-@Getter
-public class DonationReward {
+public record DonationReward(GameItem item, int price) {
     private static final String FILE = Server.getDataDirectory() + "/cfg/donation_rewards.json";
     private static List<DonationReward> rewardList;
 
     public static void load() throws IOException {
-        rewardList = JsonUtil.fromJson(FILE, new TypeToken<>() {});
+        rewardList = JsonUtil.fromJson(FILE, new TypeToken<>() {
+        });
         if (rewardList == null || rewardList.size() != 6) {
             throw new IllegalStateException(FILE + " must specify 6 items.");
         }
@@ -27,26 +27,18 @@ public class DonationReward {
         return Collections.unmodifiableList(rewardList);
     }
 
-    private final GameItem item;
-    private final int price;
-
-    public DonationReward(final GameItem item, final int price) {
-        this.item = item;
-        this.price = price;
-    }
-
     @Override
     public boolean equals(final Object o) {
         if (o == this) return true;
         if (!(o instanceof DonationReward other)) return false;
         if (!other.canEqual(this)) return false;
-        if (this.getPrice() != other.getPrice()) return false;
-        final Object this$item = this.getItem();
-        final Object other$item = other.getItem();
+        if (this.price() != other.price()) return false;
+        final Object this$item = this.item();
+        final Object other$item = other.item();
         return Objects.equals(this$item, other$item);
     }
 
-    protected boolean canEqual(final Object other) {
+    private boolean canEqual(final Object other) {
         return other instanceof DonationReward;
     }
 
@@ -54,14 +46,14 @@ public class DonationReward {
     public int hashCode() {
         final int PRIME = 59;
         int result = 1;
-        result = result * PRIME + this.getPrice();
-        final Object $item = this.getItem();
+        result = result * PRIME + this.price();
+        final Object $item = this.item();
         result = result * PRIME + ($item == null ? 43 : $item.hashCode());
         return result;
     }
 
     @Override
     public String toString() {
-        return "DonationReward(item=" + this.getItem() + ", price=" + this.getPrice() + ")";
+        return "DonationReward(item=" + this.item() + ", price=" + this.price() + ")";
     }
 }
