cf9d85af86c12955d447b4efff958928b2ecad7b
Glabay <Java_Guru@live.com>
2024-11-26 03:01:29 -0400

    Addition: adds the restClient as a Bean for the project

--- http-database/src/main/java/dev/glabay/HttpDatabase.java
+++ http-database/src/main/java/dev/glabay/HttpDatabase.java
@@ -2,6 +2,9 @@ package dev.glabay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class HttpDatabase {
@@ -10,4 +13,10 @@ public class HttpDatabase {
         SpringApplication.run(HttpDatabase.class, args);
     }
 
+    @Bean
+    RestClient restClient() {
+        return RestClient.builder()
+            .requestFactory(new JdkClientHttpRequestFactory())
+            .build();
+    }
 }
