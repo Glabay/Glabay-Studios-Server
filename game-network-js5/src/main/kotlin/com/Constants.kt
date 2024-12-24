3d32fce5a474a4946d79bfd63343c0ad4a28e1bf
Glabay <Java_Guru@live.com>
2024-12-24 01:33:24 -0400

    Rev Upgrade to 227 Data

--- game-network-js5/src/main/kotlin/com/Constants.kt
+++ game-network-js5/src/main/kotlin/com/Constants.kt
@@ -4,12 +4,15 @@ import com.custom.PackSpritesCustom
 import dev.openrune.cache.tools.tasks.CacheTask
 import dev.openrune.cache.tools.tasks.impl.PackMaps
 import dev.openrune.cache.tools.tasks.impl.PackModels
+import dev.openrune.cache.tools.tasks.impl.defs.PackConfig
+import dev.openrune.cache.tools.tasks.impl.defs.PackMode
 import io.xeros.AssetLoader
 
-const val REV : Int = 226
+const val REV : Int = 227
 
 val tasks : Array<CacheTask> = arrayOf(
     PackSpritesCustom(AssetLoader.getFolder("raw-cache/sprites/")),
     PackMaps(AssetLoader.getFolder("raw-cache/maps/")),
     PackModels(AssetLoader.getFolder("raw-cache/models/")),
+//    PackConfig(PackMode.ITEMS, AssetLoader.getFolder("raw-cache/items/"))
 )
